4fb28214fd9251df26c8902d645474bc49ca047c
stephipie <stephi.pietschmann@gmail.com>
2025-01-31 15:28:54 +0100

    add layout folder

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,20 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+
+function Layout({ children }) {
+    return (
+       <div
+       className="mt-10 flex min-h-screen w-full flex-col justify-start bg-gray-200 bg-gradient-to-bl from-[#0093E9] to-[#80D0C7]">
+        <div className="container mx-auto p-4">
+            <Header />
+            {children}
+            <Footer />
+       </div>
+       </div>
+        
+   
+    )
+}
+
+export default Layout;
---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import TodoList from "../components/TodoList";
+import Layout from "../layout/Layout";
+
+function App() {
+  return (
+      <Layout>
+      <TodoList />
+      </Layout>
+  );
+}
+
+export default App;
